c27c494af15beb63e81a65d8fab6dc2eafcc58f0
M31ch0n3 <100389338+M31ch0n3@users.noreply.github.com>
2023-11-24 14:33:57 -0300

    cargar contenido

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+    $servername='localhost';
+    $username='root';
+    $password='';
+    $dbname = "intranet";
+    $conn=mysqli_connect($servername,$username,$password,"$dbname");
+      if(!$conn){
+
+        }
+
+?>
